38b9525cfbeb92a387eac0a93ce3853e67ef2b01
thuonguyenvan <thuongnguyenvan2209@gmail.com>
2024-09-08 20:48:54 +0700

    update api to upload docs file

--- DataProcessing/main.py
+++ DataProcessing/main.py
@@ -126,10 +126,10 @@ async def get():
     return HTMLResponse(content=html_content)
 
 
-@app.post("/upload")
+@app.post("/upload_pdf")
 async def upload_pdf(file: UploadFile = File(...)):
     if file.content_type != "application/pdf":
-        raise HTTPException(status_code=400, detail="File không phải PDF")
+        raise HTTPException(status_code=400, detail="must be PDF file")
     
     file_name = f"{uuid4()}.pdf"
 
@@ -137,6 +137,22 @@ async def upload_pdf(file: UploadFile = File(...)):
     blob = bucket.blob(file_name)
     
     blob.upload_from_file(file.file, content_type="application/pdf")
+    blob.make_public()  
+
+    file_url = blob.public_url
+    return {"file_url": file_url}
+
+@app.post("/upload_docs")
+async def upload_pdf(file: UploadFile = File(...)):
+    if file.content_type != "application/docs":
+        raise HTTPException(status_code=400, detail="must be DOCS file")
+    
+    file_name = f"{uuid4()}.docs"
+
+    bucket = storage.bucket()
+    blob = bucket.blob(file_name)
+    
+    blob.upload_from_file(file.file, content_type="application/docs")
     blob.make_public()  
 
     file_url = blob.public_url
